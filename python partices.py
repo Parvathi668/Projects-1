9a4483dd8e101553c7c94f71cdf2f5714ed73b85
Parvathi668 <parvathipatil2001@gmail.com>
2025-02-07 16:27:25 +0530

    added friends

--- python partices.py
+++ python partices.py
@@ -1,12 +1,12 @@
 a =10
 def fun1():
-  a = 100
+  a = 100 #arshitha
   b = 200
   print(a)
   print(b)
 def fun2():
-  a=1000
-  c= 30
+  a=1000 # parvathipatil
+  c= 30 #navitha
   print(c)
   print(a)
 print(a)
